aca2778b49cea02e6a780eb1c62ce06fded98c4f
aberk <aberk@us.ibm.com>
2017-12-07 16:33:12 -0500

    Removed Debug Statements

--- src/views/app.js
+++ src/views/app.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Debug from 'debug';
 import ReactDOM from 'react-dom';
 
 import PickerPage from './PickerPage';
-
-var debug = Debug('myApp');
 
 /*
  * @class App
@@ -16,7 +13,6 @@ class App {
    * @param {Object} options
    */
   constructor(options) {
-    debug('create app with options', options);
 
     this.state = options.state;
   }
@@ -28,8 +24,6 @@ class App {
    */
    render (element) {
 
-    debug('render app with state', this.state);
-
     // would be in JSX: <AppRoot state={this.state} />
     var appRootElement = React.createElement(PickerPage, {
       state: this.state
@@ -37,13 +31,11 @@ class App {
 
     // render to DOM
     if(element) {
-      debug('render to DOM');
       ReactDOM.render(appRootElement, element);
       return;
     }
 
     // render to string
-    debug('render to string');
     return ReactDOM.renderToString(appRootElement);
   }
 
@@ -53,7 +45,7 @@ class App {
    */
    renderToDOM (element) {
     if(!element) {
-      return debug(new Error('App.renderToDOM: element is required'));
+      return new Error('App.renderToDOM: element is required');
     }
 
     this.render(element);
